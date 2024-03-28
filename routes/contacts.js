6a6818f93398b97c0cfe33a6fb1f58b903bc42f9
Ashfaq Haneef <78129119+mrthangu@users.noreply.github.com>
2024-03-28 09:52:50 +0530

    updated delete

--- routes/contacts.js
+++ routes/contacts.js
@@ -13,6 +13,6 @@ router.get("/api/contacts", getAllContacts);
 router.get("/api/contacts/:id", getAContact);
 router.post("/api/contacts", createAContact);
 router.put("/api/contacts/:id", updateAContact);
-router.delete("/api/contacts", deleteAContact);
+router.delete("/api/contacts/:id", deleteAContact);
 
 export default router;
